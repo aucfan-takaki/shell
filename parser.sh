594c140d6f96e8dbf0728cc3d79247fd840dd2da
takaki <takaki@AFN-takaki.local>
2014-09-10 10:04:28 +0900

    桁数制御をseq→printf

--- parser.sh
+++ parser.sh
@@ -4,26 +4,16 @@ cd `dirname $0`
 
 #cat cache/amespi-5664.html
 
-url=cache/dragonballgt-5580.html
+url=cache/nishikori-5684.html
 
 grep "class=\"entry-title" $url | sed -e "s/\t\t<h1 class=\"entry-title\">//g" -e "s/<\/h1>\t\t<div class=\"entry-meta post_meta_box\">//g"
 
 year=`grep "class=\"sep" $url | grep -o "[0-9]*年" | grep -o "[0-9]*"`
 
-echo $year
-
 month=`grep "class=\"sep" $url | grep -o "[0-9]*月" | grep -o "[0-9]*"`
-
-month=`seq -f %02g $month $month`
-
-echo $month
 
 day=`grep "class=\"sep" $url | grep -o "[0-9]*日" | grep -o "[0-9]*"`
 
-day=`seq -f %02g $day $day`
-
-echo $day
-
-time=$year$month$day
+time=`printf "%04d%02d%02d" $year $month $day`
 
 echo $time
