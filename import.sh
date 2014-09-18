c291e63730e75762ae800581711797d7f1fb697b
takaki <takaki@AFN-takaki.local>
2014-09-18 18:05:26 +0900

    クローラー完成

--- import.sh
+++ import.sh
@@ -4,10 +4,11 @@
 
 #create table okutopi(date int,title text,url text);
 
+#show variables like "char%";
 #set character_set_results=utf8;
 #mysql -uroot -e "use calendar;"
 
-mysql -uroot -e "use calendar; LOAD DATA INFILE '/deploy/shell/cache/tsv.tsv' INTO TABLE okutopi (date,title,url);"
+mysql -uroot -e "set character_set_results=utf8; use calendar; LOAD DATA LOCAL INFILE '/deploy/shell/cache/tsv.tsv' INTO TABLE okutopi (date,title,url);"
 
 #\\q
 
