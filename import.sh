dd91542c2c02bbe3b6e08788f1eec08ffff634e4
takaki <takaki@AFN-takaki.local>
2014-09-18 18:32:02 +0900

    sh一部修正

--- import.sh
+++ import.sh
@@ -8,7 +8,7 @@
 #set character_set_results=utf8;
 #mysql -uroot -e "use calendar;"
 
-mysql -uroot -e "set character_set_results=utf8; use calendar; LOAD DATA LOCAL INFILE '/deploy/shell/cache/tsv.tsv' INTO TABLE okutopi (date,title,url);"
+mysql -uroot -e "use calendar; set character_set_results=utf8; LOAD DATA LOCAL INFILE '/deploy/shell/cache/tsv.tsv' INTO TABLE okutopi (date,title,url);"
 
 #\\q
 
